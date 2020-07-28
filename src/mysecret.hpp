becb7c47bdcd50599a0392102df1c0c93ac0f1a5
Mohammad Shams <gr8linux@yahoo.com>
2020-07-28 20:09:38 +0430

    Fix mysecret

---
+++ src/mysecret.hpp
@@ -0,0 +1,11 @@
+
+//************************************
+// Replace with your wifi credentials
+const char * ssid = "xxxxxxxxxx";
+const char * password = "xxxxxxxxxx";
+
+// Get and API Key by registering on
+// https://openweathermap.org/api
+// 
+
+String Key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
